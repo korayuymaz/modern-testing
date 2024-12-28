c6b50b4a59a58d2dfa7473847f09f2d369ad811e
Koray <koray.uymaz@agesa.com.tr>
2024-12-28 18:37:34 +0300

    waitFor usage for testing a DOM update

--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { render, screen } from "@testing-library/react";
+import { waitFor, render, screen } from "@testing-library/react";
 
-// function TestComponent({ products }) {
-//   return (
-//     <div>
-//       <label for="user-name">User Name:</label>
-//       <input id="user-name" placeholder="Enter Your User Name" />
-//       <button>Test</button>
-//       <ul>
-//         {products.map((product, index) => (
-//           <li key={index}>{product}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// function TestComponentTwo({ suffix }) {
-//   return <div>{suffix ? suffix : "Suffix not exist"}</div>;
-// }
-
-function TestComponentAsync() {
-  const [message, setMessage] = useState("First Message");
+function TestComponent() {
+  const [message, setMessage] = useState("First");
 
   useEffect(() => {
     setTimeout(() => {
-      setMessage("Second Message");
+      setMessage("Second");
     }, [300]);
   }, []);
   return (
@@ -35,36 +16,16 @@ function TestComponentAsync() {
   );
 }
 
-// it("should render the element correctly", () => {
-//   const products = ["product1", "product2", "product3"];
-//   render(<TestComponent products={products} />);
-//   const listItems = screen.getAllByRole("listitem");
-//   const buttonElement = screen.getByRole("button", {
-//     name: "Test",
-//   }); //Role used for HTML Elements tag names and role names may differ
-//   const labelElement = screen.getByLabelText("User Name:");
-//   const inputElement = screen.getByPlaceholderText("Enter Your User Name");
-
-//   expect(labelElement).toBeInTheDocument();
-//   expect(inputElement).toBeInTheDocument();
-//   expect(buttonElement).toBeInTheDocument();
-//   expect(listItems).toHaveLength(products.length);
-// });
-
-// it("should render the second test element correctly", () => {
-//   render(<TestComponentTwo suffix={"Test"} />);
-//   const element = screen.getByText("Test");
-//   const emptyElement = screen.queryByText("Suffix not exist"); // If the element does not exist query is used
-//   expect(emptyElement).not.toBeInTheDocument();
-//   expect(element).toBeInTheDocument();
-//   // const element = screen.getByText("Hello", {
-//   //   exact: false,
-//   // });
-//   // expect(element).toBeInTheDocument();
-// });
-
 it("should render the third test correctly", async () => {
-  render(<TestComponentAsync />);
-  const element = await screen.findByText(/Second/i);
-  expect(element).toBeInTheDocument();
+  render(<TestComponent />);
+  // const elementExist = await screen.findByText(/Second/i);
+  // const elementShouldNotExist = screen.queryByText("First");
+  // expect(elementExist).toBeInTheDocument();
+  // expect(elementShouldNotExist).not.toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.getByText("Second")).toBeInTheDocument();
+  });
+  await waitFor(() => {
+    expect(screen.queryByText("First")).not.toBeInTheDocument();
+  });
 });
