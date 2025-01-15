0fd7f4a06900860ad65a5d331f0310cb52187425
Koray <koray.uymaz@agesa.com.tr>
2025-01-15 20:53:17 +0300

    Using hooks in testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { screen } from "@testing-library/react";
+import { render, renderHook, screen, act } from "@testing-library/react";
 import { customRender } from "./test-utils";
 
-function TestComponent() {
-  return (
-    <div>
-      <p>TestComponent</p>
-    </div>
-  );
+function useCustomHook() {
+  const [name, setName] = React.useState("John Doe");
+  const changeName = (newName) => setName(newName);
+
+  return { name, changeName };
 }
 
 it("should render TestComponent", async () => {
-  customRender(<TestComponent />);
-  expect(screen.getByText("TestComponent")).toBeInTheDocument();
+  const { result } = renderHook(useCustomHook);
+
+  expect(result.current.name).toBe("John Doe");
+
+  act(() => {
+    result.current.changeName("Jane Doe");
+  });
+
+  expect(result.current.name).toBe("Jane Doe");
 });
--- src/test-utils.js
+++ src/test-utils.js
@@ -4,14 +4,14 @@ function ThemeProvider({ children }) {
   return <div className="theme-provider">{children}</div>;
 }
 
-function i18nProvider({ children }) {
+function I18nProvider({ children }) {
   return <div className="i18n-provider">{children}</div>;
 }
 
 function AllProviders({ children }) {
   return (
     <ThemeProvider>
-      <i18nProvider>{children}</i18nProvider>
+      <I18nProvider>{children}</I18nProvider>
     </ThemeProvider>
   );
 }
