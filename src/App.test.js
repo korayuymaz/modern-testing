a815e32a2e1d59d9c60231dbfc4a1b657626d881
Koray <koray.uymaz@agesa.com.tr>
2025-01-17 12:20:52 +0300

    Using Providers in testing with customRender demo

--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,16 @@
-import React, { useState } from "react";
-import userEvent from "@testing-library/user-event";
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { screen } from "@testing-library/react";
+import { customRender } from "./test-utils";
 
 function TestComponent() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={handleIncrement}>Increment</button>
+      <p>TestComponent</p>
     </div>
   );
 }
 
-it("should render the third test correctly", async () => {
-  const user = userEvent.setup();
-  render(<TestComponent />);
-  await user.pointer({
-    keys: "[MouseLeft]",
-    target: screen.getByRole("button", { name: "Increment" }),
-  });
-
-  const headingElement = await screen.findByRole("heading");
-
-  expect(headingElement).toHaveTextContent("1");
+it("should render TestComponent", async () => {
+  customRender(<TestComponent />);
+  expect(screen.getByText("TestComponent")).toBeInTheDocument();
 });
